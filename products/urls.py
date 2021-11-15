cb5c67ab527c747e5f3f7bb5c513e0e125a888bc
Mohamed Elhwary <mh.hwary@yahoo.com>
2021-11-15 03:50:21 +0200

    test building the products CRUD

---
+++ products/urls.py
@@ -0,0 +1,14 @@
+from . views import ProductViewSet
+from django.urls import path
+
+urlpatterns = [
+    path('products/', ProductViewSet.as_view({
+        'get' : 'list',
+        'post' : 'create',
+    })),
+    path('products/<str:pk>', ProductViewSet.as_view({
+        'get' : 'retirieve',
+        'put' : 'update',
+        'delete' : 'destroy',
+    }) )
+]
