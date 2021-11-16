edd943e14a921cdfe5bb1ac089773a983f3c140a
Mohamed Elhwary <mh.hwary@yahoo.com>
2021-11-16 18:55:54 +0200

    tests for list, delete products

--- products/tests.py
+++ products/tests.py
@@ -17,6 +17,7 @@ class ProductTests(APITestCase):
             "image" : "book test cases image"
         }
         response = self.client.post(url, data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Product.objects.count(), 1)
         self.assertEqual(Product.objects.get().title, 'Book test cases')
@@ -25,14 +26,34 @@ class ProductTests(APITestCase):
     def test_retrieve_product(self):
         product = Product.objects.create(title='Book test cases1', image='book test cases image1')
         product.save()
-        # product_id = Product.objects.first().id
+
         url = '/api/products/{0}'.format(product.id)
         response = self.client.get(url)
-        data = {
-            "title" : "Book test cases1",
-            "image" : "book test cases image1"
-        }
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
         result = json.loads(response.content)        
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(result['title'], product.title)
     
+    
+    def test_list_products(self):
+        product = Product.objects.create(title='Book test cases1', image='book test cases image1')
+        product.save()
+        product = Product.objects.create(title='Book test cases2', image='book test cases image2')
+        product.save()
+
+        url = '/api/products/'
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Product.objects.count(), 2)    # test the 2 created objects
+
+
+    def test_delete_product(self):
+        product = Product.objects.create(title='Book test cases1', image='book test cases image1')
+        product.save()
+
+        url = '/api/products/{0}'.format(product.id)
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
