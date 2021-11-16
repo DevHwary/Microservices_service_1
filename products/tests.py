f84ecd46f2125c9102190686342f79e84d236387
Mohamed Elhwary <mh.hwary@yahoo.com>
2021-11-16 18:41:20 +0200

    tests for retrieve product

--- products/tests.py
+++ products/tests.py
@@ -6,6 +6,7 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
 from .models import Product
+import json
 
 
 class ProductTests(APITestCase):
@@ -14,8 +15,24 @@ class ProductTests(APITestCase):
         data = {
             "title" : "Book test cases",
             "image" : "book test cases image"
-            }
+        }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Product.objects.count(), 1)
         self.assertEqual(Product.objects.get().title, 'Book test cases')
+
+
+    def test_retrieve_product(self):
+        product = Product.objects.create(title='Book test cases1', image='book test cases image1')
+        product.save()
+        # product_id = Product.objects.first().id
+        url = '/api/products/{0}'.format(product.id)
+        response = self.client.get(url)
+        data = {
+            "title" : "Book test cases1",
+            "image" : "book test cases image1"
+        }
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)        
+        self.assertEqual(result['title'], product.title)
+    
