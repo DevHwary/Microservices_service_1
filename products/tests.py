6d624e5cff5f2f61a9747d6894701afa4a93207b
Mohamed Elhwary <mh.hwary@yahoo.com>
2021-11-16 18:22:54 +0200

    tests for create product

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django import urls
+from django.conf.urls import url
+from django.http import response
+from django.test import client
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import Product
 
-# Create your tests here.
+
+class ProductTests(APITestCase):
+    def test_create_product(self):
+        url = '/api/products/'
+        data = {
+            "title" : "Book test cases",
+            "image" : "book test cases image"
+            }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertEqual(Product.objects.get().title, 'Book test cases')
